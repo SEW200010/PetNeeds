9b559d8ef4676888697a8056059072dea33a254c
malshi-kavindya <malshikavi.w@gmail.com>
2025-06-21 20:44:20 +0530

    handle routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import Aboutus from "./pages/Aboutus";
 import Team from "./pages/Team";
 import Services from "./pages/Services";
 import Register from "./pages/Register";
+import User from "./pages/User/User";
+import Admin from "./pages/Admin/Admin";
+
+
 
 const App = () => {
   return (
@@ -18,7 +22,9 @@ const App = () => {
         <Route path="/Services" element={<Services />}/>
         <Route path="/Aboutus" element={<Aboutus />}/>
         <Route path="/Register" element={<Register />}/>
-        <Route path="/admin-dashboard" element={<Team />}/>
+        <Route path="/user-dashboard" element={<User />}/>
+        <Route path="/admin-dashboard" element={<Admin />}/>
+        
         {/* Add more routes here for About, Services, etc. */}
       </Routes>
     </Router>
