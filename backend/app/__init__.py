333b2b7f9e443c34f39957806c67b8e4de92b868
malshi-kavindya <malshikavi.w@gmail.com>
2025-07-03 12:37:43 +0530

    Base URL for DB

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+#from dotenv import load_dotenv
+#import os
+
+# Load variables from .env file
+# load_dotenv()
 
 mongo = PyMongo()
 
@@ -8,6 +13,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
+    # app.config["MONGO_URI"] = os.getenv("DB_URI")
     app.config["MONGO_URI"] = "mongodb://localhost:27017/lifeskill"
     mongo.init_app(app)
 
