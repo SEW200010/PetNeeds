2eaca0a9cdc88f8ffaf289ae83232edaea52a7b9
malshi-kavindya <malshikavi.w@gmail.com>
2025-10-19 17:14:59 +0530

    logo replacement

--- frontend/src/components/Coordinator/CoordinatorHeader.jsx
+++ frontend/src/components/Coordinator/CoordinatorHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logoNew.png";
 import { Menu as MenuIcon, LogOut as LogOutIcon } from "lucide-react";
 import { logout } from "../../lib/utils";
 import Badge from "@mui/material/Badge";
