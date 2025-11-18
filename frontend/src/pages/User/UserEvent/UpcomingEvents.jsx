8cdc7a59be2b6da82ec516f8441b4bef855586b3
Bihari-Fernando <biharifernando2000@gmail.com>
2025-11-18 14:41:43 +0530

    add join button disabling mechanism to event card

--- frontend/src/pages/User/UserEvent/UpcomingEvents.jsx
+++ frontend/src/pages/User/UserEvent/UpcomingEvents.jsx
@@ -147,6 +147,7 @@ export default function UpcomingEvents() {
                     event={event}
                     userId={user._id}
                     onJoin={handleJoin}
+                    showJoinButton={false}
                   />
                 ))
               ) : (
